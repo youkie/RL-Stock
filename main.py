00e3d5b96b833409c581753ca0d570d08421f567
youkie <892861337@qq.com>
2020-11-18 18:40:55 +0800

    Update main.py
    fix a bug

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def stock_trade(stock_file):
     # The algorithms require a vectorized environment to run
     env = DummyVecEnv([lambda: StockTradingEnv(df)])
 
-    model = PPO2(MlpPolicy, env, verbose=0, tensorboard_log='./log')
+    model = PPO('MlpPolicy', env, verbose=0, tensorboard_log='./log')
     model.learn(total_timesteps=int(1e6))
 
     df_test = pd.read_csv(stock_file.replace('train', 'test'))
